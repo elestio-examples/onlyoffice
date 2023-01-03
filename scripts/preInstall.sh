775a35ee61d0802fde03bf79d8a8660d303705e5
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-01-03 17:03:08 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -26,19 +26,19 @@ mkdir -p "app/onlyoffice/ControlPanel/logs"
 
 mkdir -p "sys/fs/cgroup"
 
-# echo "[mysqld]
-# sql_mode = 'NO_ENGINE_SUBSTITUTION'
-# max_connections = 1000
-# max_allowed_packet = 1048576000
-# group_concat_max_len = 2048
-# log-error = /var/log/mysql/error.log" > ./app/onlyoffice/mysql/conf.d/onlyoffice.cnf
+echo "[mysqld]
+sql_mode = 'NO_ENGINE_SUBSTITUTION'
+max_connections = 1000
+max_allowed_packet = 1048576000
+group_concat_max_len = 2048
+log-error = /var/log/mysql/error.log" > ./app/onlyoffice/mysql/conf.d/onlyoffice.cnf
 
-# echo "CREATE USER 'onlyoffice_user'@'localhost' IDENTIFIED BY 'onlyoffice_pass';
-# CREATE USER 'mail_admin'@'localhost' IDENTIFIED BY 'Isadmin123';
-# GRANT ALL PRIVILEGES ON * . * TO 'root'@'%' IDENTIFIED BY 'my-secret-pw';
-# GRANT ALL PRIVILEGES ON * . * TO 'onlyoffice_user'@'%' IDENTIFIED BY 'onlyoffice_pass';
-# GRANT ALL PRIVILEGES ON * . * TO 'mail_admin'@'%' IDENTIFIED BY 'Isadmin123';
-# FLUSH PRIVILEGES;" > ./app/onlyoffice/mysql/initdb/setup.sql
+echo "CREATE USER 'onlyoffice_user'@'localhost' IDENTIFIED BY 'onlyoffice_pass';
+CREATE USER 'mail_admin'@'localhost' IDENTIFIED BY 'Isadmin123';
+GRANT ALL PRIVILEGES ON * . * TO 'root'@'%' IDENTIFIED BY 'my-secret-pw';
+GRANT ALL PRIVILEGES ON * . * TO 'onlyoffice_user'@'%' IDENTIFIED BY 'onlyoffice_pass';
+GRANT ALL PRIVILEGES ON * . * TO 'mail_admin'@'%' IDENTIFIED BY 'Isadmin123';
+FLUSH PRIVILEGES;" > ./app/onlyoffice/mysql/initdb/setup.sql
 
 
 # mkdir mysql_data
